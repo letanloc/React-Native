92d51ef0b20186e8e3d207d1bc0644ac94099185
locle <letanloc1941995@gmail.com>
2019-01-09 17:21:19 +0700

    flex and form

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, Input, Form, Container, Header, Item} from 'native-base'
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,16 +19,79 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
     render() {
         return (
-            <View>
-                <Text>LOC DEP TRAI</Text>
-            </View>
+            <Container>
+                <Header></Header>
+                <View style={styles.mainbox}>
+                    <View style={styles.box1}>
+                        <Text style={styles.tex1}>HÈ LÚ</Text>
+                    </View>
+                    <View style={styles.box2}>
+                        <View style={{flex: 1, backgroundColor: "#fff"}}>
+                            <View style={{width: 50, height: 50, backgroundColor: "#8f9bff"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#0043ca"}}/>
+                        </View>
+                        <View style={{flex: 1, flexDirection: "row"}}>
+                            <View style={{width: 50, height: 50, backgroundColor: "#8f9bff"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#0043ca"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#8f9bff"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#0043ca"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#8f9bff"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#0043ca"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#8f9bff"}}/>
+                            <View style={{width: 50, height: 50, backgroundColor: "#0043ca"}}/>
+                        </View>
+                    </View>
+                    <View style={styles.box3}>
+                        <Form>
+                            {/*last fulll*/}
+                            <Item last>
+                                <Input placeholder="Username"/>
+                            </Item>
+
+                            <Item last>
+                                <Input placeholder="Password"/>
+                            </Item>
+
+                        </Form>
+                        {/*<Input placeholder="username"></Input>*/}
+                        {/*<Button light><Text> Light </Text></Button>*/}
+                    </View>
+                </View>
+            </Container>
+
         );
     }
 }
 
 const styles = StyleSheet.create({
+    mainbox: {
+        flex: 1,
+
+    },
+    box1: {
+        flex: 1,
+        backgroundColor: "green",
+        textAlign: 'center',
+        justifyContent: 'center',//center vertical
+        alignItems: 'center',// center horizontal
+    },
+
+    tex1: {
+        color: '#FFFFFF',
+        fontSize: 50
+    },
+
+    box2: {
+        flex: 2,
+        flexDirection: 'column',
+        backgroundColor: '#62efff'
+    },
+    box3: {
+        flex: 3,
+    },
 
     container: {
         flex: 1,
